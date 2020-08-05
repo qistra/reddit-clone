b83052c68bf99125443f2f71e04452dc40f1074e
Qi <>
2020-08-05 13:33:06 +0200

    implement more APIs for Posts

--- src/main/java/com/example/redditclone/controller/PostController.java
+++ src/main/java/com/example/redditclone/controller/PostController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.ws.rs.Path;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/posts")
 @AllArgsConstructor
@@ -25,5 +28,23 @@ public class PostController {
     public ResponseEntity<PostResponse> getPost(@PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(postService.getPost(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(postService.getAllPosts());
+    }
+
+    @GetMapping("/subreddit/{id}")
+    public ResponseEntity<List<PostResponse>> getPostsBySubreddit(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(postService.getPostsBySubreddit(id));
+    }
+
+    @GetMapping("/user/{name}")
+    public ResponseEntity<List<PostResponse>> getPostsByUser(@PathVariable String username) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(postService.getPostsByUser(username));
     }
 }
--- src/main/java/com/example/redditclone/service/PostService.java
+++ src/main/java/com/example/redditclone/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.redditclone.service;
 import com.example.redditclone.dto.PostRequest;
 import com.example.redditclone.dto.PostResponse;
 import com.example.redditclone.exception.PostNotFoundException;
+import com.example.redditclone.exception.SpringRedditException;
 import com.example.redditclone.exception.SubredditNotFoundException;
 import com.example.redditclone.mapper.PostMapper;
 import com.example.redditclone.model.Post;
@@ -10,11 +11,14 @@ import com.example.redditclone.model.Subreddit;
 import com.example.redditclone.model.User;
 import com.example.redditclone.repository.PostRepository;
 import com.example.redditclone.repository.SubredditRepository;
+import com.example.redditclone.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -23,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostService {
     private final PostRepository postRepository;
     private final SubredditRepository subredditRepository;
+    private final UserRepository userRepository;
     private final AuthService authService;
     private final PostMapper postMapper;
 
@@ -37,10 +42,42 @@ public class PostService {
         return postMapper.mapToResponse(postRepository.save(post));
     }
 
+    @Transactional(readOnly = true)
     public PostResponse getPost(Long id) {
         Post post = postRepository.findById(id)
                 .orElseThrow(() -> new PostNotFoundException(id.toString()));
 
         return postMapper.mapToResponse(post);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getAllPosts() {
+        return postRepository.findAll()
+                .stream()
+                .map(postMapper::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getPostsBySubreddit(Long subredditId) {
+        Subreddit subreddit = subredditRepository.findById(subredditId)
+                .orElseThrow(() -> new SubredditNotFoundException(subredditId.toString()));
+
+        return postRepository.findAllBySubreddit(subreddit)
+                .stream()
+                .map(postMapper::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostResponse> getPostsByUser(String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new SpringRedditException("User with username " + username + " not found"));
+
+        return postRepository.findAllByUser(user)
+                .stream()
+                .map(postMapper::mapToResponse)
+                .collect(Collectors.toList());
+
     }
 }
